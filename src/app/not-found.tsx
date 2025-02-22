15ea4fe8b9e09f7b09a53972f196921f4c8b3a02
Saša Radovanović <sasaradovanovic@192.168.1.8>
2025-02-22 18:24:24 +0100

    added not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+  return (
+    <div className='space-y-5 text-center'>
+      <h2 className='text-2xl'>UPS, GREŠKA 404</h2>
+      <p className='text-xl font-semibold'>
+        Izvinjavamo se, stranica nije pronađena.
+      </p>
+      <p>Link koji ste pratili je neispravan ili je stranica uklonjena.</p>
+    </div>
+  )
+}
